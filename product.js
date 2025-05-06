31ede7683320f5da094f6a48277ba631f8c8d07c
lekkyj <lekanjayeola0@gmail.com>
2025-05-06 15:41:29 +0100

    Create product.js

---
+++ product.js
@@ -0,0 +1,9 @@
+// src/product.js
+const product = {
+  name: "Super Gadget",
+  price: "$199.99",
+  description: "A top-notch gadget with amazing features for tech lovers.",
+  image: "https://via.placeholder.com/300"
+};
+
+export default product;
